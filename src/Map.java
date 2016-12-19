c047e63162e1f976d01dc613e47d1b80980ec826
sb <taekgu04@naver.com>
2016-12-19 23:05:22 +0900

    nn

--- src/Map.java
+++ src/Map.java
@@ -11,6 +11,7 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -30,6 +31,10 @@ public class Map extends JFrame{
 	private JButton D_Button = new JButton();
 	private JButton U_Button = new JButton();
 	private JButton T_Button = new JButton();
+	
+	
+	public gui g;
+	
 	//--------------------------------
 
 	JPanel map_panel;
@@ -41,108 +46,36 @@ public class Map extends JFrame{
     public Map(){
     	icon = new ImageIcon("Seoul.JPG");
     }
-    
-    public JPanel Map_init(){
+        
+    public JPanel Map_init(boolean temp_f){
     	map_panel = new JPanel() {
 			public void paintComponent(Graphics g) {
-                g.drawImage(icon.getImage(), 100, 0, 1820,1000,this);
+                g.drawImage(icon.getImage(), 0, 0, 1820,1000,this);
             };
 
     	};
     	map_panel.setLayout(null);
     	    
-    	setUser();
+    	if(temp_f == true){
+    		setMAap(100,100,0);
+    		setMAap(200,200,0);
+    		setMAap(300,300,0);
+    	}else if(temp_f == false){
+    		jb[0].setVisible(false);
+    		
+    	}
+    	
     	
     	return map_panel;
     }
-    
-    public void setUser(){
-    	
-    	D_Button.setText("전체 날짜");
-    	D_Button.setBounds(0, 0, 100, 50);
-    	D_Button.addActionListener(new MyActionListener());
-		
-    	combo_d.addItem("date1");
-    	combo_d.setBounds(0, 50, 100, 50);
-		combo_d.addItem("date2");
-		combo_d.addItem("date3");
-		combo_d.setVisible(false);
-		
-		U_Button.setText("전체User");
-		U_Button.setBounds(0, 500, 100, 50);
-		U_Button.addActionListener(new MyActionListener());
-    	
-		combo_u.addItem("user1");
-		combo_u.setBounds(0, 550, 100, 50);
-		combo_u.addItem("user2");
-		combo_u.addItem("user3");
-		combo_u.setVisible(false);
-    	
-		T_Button.setText("전체 온도");
-		T_Button.setBounds(0, 950, 100, 50);
-		T_Button.addActionListener(new MyActionListener());
-		
-		map_panel.add(D_Button);
-		map_panel.add(U_Button);
-		map_panel.add(T_Button);
-		map_panel.add(combo_d);
-		map_panel.add(combo_u);
-    }
-    
-    private class MyActionListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-        	//------------------date-----------------
-            JButton b = (JButton) e.getSource();
-            if (b.getText().equals("전체 날짜")){
-            	b.setText("날짜 선택");
-            	combo_d.setVisible(true);
-            }      	
-            else if(b.getText().equals("날짜 선택")){
-            	b.setText("전체 날짜");
-            	setTitle(b.getText());
-            	combo_d.setVisible(false);
-            	// InnerClassListener의 멤버나 JFrame의 멤버 호출
-            }
-            //---------------user---------------------
-            if (b.getText().equals("전체User")){
-            	b.setText("User 선택");
-            	combo_u.setVisible(true);
-            }      	
-            else if(b.getText().equals("User 선택")){
-            	b.setText("전체User");
-            	setTitle(b.getText());
-            	combo_u.setVisible(false);
-            	// InnerClassListener의 멤버나 JFrame의 멤버 호출
-            }
-            
-            //--------------temp-----------------
-            if (b.getText().equals("전체 온도")){
-            	b.setText("고온");
-            	//combo.setVisible(true);
-            }      	
-            else if(b.getText().equals("고온")){
-            	b.setText("전체 온도");
-            	setTitle(b.getText());
-            	//combo.setVisible(false);
-            	// InnerClassListener의 멤버나 JFrame의 멤버 호출
-            }
-
-        }
-    }
-    
-   
-    public void setMAap(int a, int b){
-    	
-    	jb[0] = new JButton(new ImageIcon("check.png"));
-    	jb[0].setBounds(a+100, b, 30, 30);
-    	jb[0].setBorderPainted(false);
-    	jb[0].setContentAreaFilled(false);
-    	//jb[b_num].setSize(30,30);
-        //jb[b_num].setLocation(a,b);
-    	map_panel.add(jb[0]);
-        b_num++;
           
+    public void setMAap(int a, int b, int cnt){
+    	
+    	jb[cnt] = new JButton(new ImageIcon("check.png"));
+    	jb[cnt].setBounds(a+100, b, 30, 30);
+    	jb[cnt].setBorderPainted(false);
+    	jb[cnt].setContentAreaFilled(false);
+    	map_panel.add(jb[cnt]);
     }
-        
   
 }
--- src/gui.java
+++ src/gui.java
@@ -3,13 +3,17 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
 import java.io.IOException;
 import java.sql.SQLException;
 
 import javax.swing.ComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,34 +23,47 @@ import javax.swing.JTabbedPane;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 
+
 public class gui extends JFrame {
 	
 	private static final long serialVersionUID = 5193460907470526697L;
 	
-	private JFrame jF;
-	private JTabbedPane tabbedPane;
+	public JFrame jF;
+	public JTabbedPane tabbedPane;
 	private JPanel panel1;
-	private JPanel panel2;
+	public JPanel panel2;
 	private JPanel panel3;
+	private JPanel Map_panel;
+	
 	//private JLabel label1;
 	private JLabel label2;
 	private JLabel label3;
 	
-	//------------------MAP-----------------------
 	
-	private JComboBox combo = new JComboBox();
-	private JLabel label = new JLabel();
+	//------------------MAP-----------------------
+		
+	private JComboBox combo_d = new JComboBox();
+	private JComboBox combo_u = new JComboBox();
+	private JButton D_Button = new JButton();
+	private JButton U_Button = new JButton();
+	private JButton T_Button = new JButton();
+	
+	ImageIcon icon;
+	public Map p2;
 	//--------------------------------
 
 	
 	public gui() throws IOException, SQLException
 	{
 		//���� ������ ����
+		
+		icon = new ImageIcon("Seoul.JPG");
 
 		tabbedPane  = new JTabbedPane();
 				
 		panel1 = new JPanel();
 		panel2 = new JPanel();
+		panel2.setLayout(null);
 		panel3 = new JPanel();
 		
 		
@@ -56,8 +73,8 @@ public class gui extends JFrame {
 		
 		// add chart in JPanel
 		jPanel01 jP = new jPanel01();
-		Map p2 = new Map();
-		panel2 = p2.Map_init();
+		p2 = new Map();
+		//panel2 = p2.Map_init();
 		panel1.setLayout(new java.awt.BorderLayout());
 		
 	
@@ -69,23 +86,9 @@ public class gui extends JFrame {
 		
 		
 		//--------MAP--------------------------------------------------------------------------
+						
+		setUser();
 		
-		
-		label.setText("사용자 목록");
-		
-		combo.addItem("user1");
-		combo.addItem("user2");
-		combo.addItem("user3");
-		
-		panel2.add(label);
-		panel2.add(combo);
-		//combo.addActionListener((ActionListener) this);
-		
-		//DATA MAT SHOW
-		p2.setMAap(100, 100);
-		p2.setMAap(200, 200);
-		p2.setMAap(300, 300);
-		p2.setMAap(105, 105);
 		
 		//----------------------------------------------------------------------------------------------
 
@@ -113,8 +116,115 @@ public class gui extends JFrame {
 	
 	}
 	
+	public void setUser(){
+		
+    	D_Button.setText("전체 날짜");
+    	D_Button.setBounds(0, 0, 100, 50);
+    	D_Button.addActionListener(new MyActionListener());
+		
+    	combo_d.addItem("date1");
+    	combo_d.setBounds(0, 50, 100, 50);
+		combo_d.addItem("date2");
+		combo_d.addItem("date3");
+		combo_d.setVisible(false);
+		
+		U_Button.setText("전체User");
+		U_Button.setBounds(0, 500, 100, 50);
+		U_Button.addActionListener(new MyActionListener());
+    	
+		combo_u.addItem("user1");
+		combo_u.setBounds(0, 550, 100, 50);
+		combo_u.addItem("user2");
+		combo_u.addItem("user3");
+		combo_u.setVisible(false);
+    	
+		T_Button.setText("전체 온도");
+		T_Button.setBounds(0, 950, 100, 50);
+		T_Button.addActionListener(new MyActionListener());
+		
+		panel2.add(D_Button);
+		panel2.add(U_Button);
+		panel2.add(T_Button);
+		panel2.add(combo_d);
+		panel2.add(combo_u);
+    }
+    
+	
+    private class MyActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        	//------------------date-----------------
+            JButton b = (JButton) e.getSource();
+            if (b.getText().equals("전체 날짜")){
+            	b.setText("날짜 선택");
+            	combo_d.setVisible(true);
+            }      	
+            else if(b.getText().equals("날짜 선택")){
+            	b.setText("전체 날짜");
+            	setTitle(b.getText());
+            	combo_d.setVisible(false);
+            	// InnerClassListener의 멤버나 JFrame의 멤버 호출
+            }
+            //---------------user---------------------
+            if (b.getText().equals("전체User")){
+            	b.setText("User 선택");
+            	combo_u.setVisible(true);
+            }      	
+            else if(b.getText().equals("User 선택")){
+            	b.setText("전체User");
+            	setTitle(b.getText());
+            	combo_u.setVisible(false);
+            	// InnerClassListener의 멤버나 JFrame의 멤버 호출
+            }
+            
+            //--------------temp-----------------
+            if (b.getText().equals("전체 온도")){
+            	b.setText("고온");
+            	//combo.setVisible(true);
+            	showMap(true);
+      
+            }      	
+            else if(b.getText().equals("고온")){
+            	b.setText("전체 온도");
+            	setTitle(b.getText());
+            	//combo.setVisible(false);
+            	// InnerClassListener의 멤버나 JFrame의 멤버 호출
+            	showMap2(true);
+            }
 
-
+        }
+    }
+    
+    private void showMap(boolean temp_f){
+    	
+    	p2 = new Map();
+		Map_panel = p2.Map_init(temp_f);
+		Map_panel.setBounds(100, 0, 1820, 1000);
+		
+		panel2.add(Map_panel);
+		tabbedPane.add("Map", panel2);
+		tabbedPane.add("Third", panel3);
+		jF.add(tabbedPane);
+    	
+    }
+    
+private void showMap2(boolean temp_f){
+	
+		jF.removeAll();
+		
+		panel2.revalidate();
+		panel2.repaint();
+		p2 = new Map();
+		Map_panel = p2.Map_init(temp_f);
+		Map_panel.setBounds(0, 0, 1820, 1000);
+		
+		tabbedPane.add("testst", panel2);
+		
+		jF.add(tabbedPane);
+		//this.setContentPane(jF);
+    	
+    }
+    
+  
 
 	public void start() throws SQLException
 	{
