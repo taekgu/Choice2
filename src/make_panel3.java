6a4cf8fa81dea5882c3ece2f4d2041ecb35cf1f1
leehy0321 <sgn0321@naver.com>
2016-12-26 12:14:16 +0900

    HY - 구분에 지역까지 추가했습니당

--- src/make_panel3.java
+++ src/make_panel3.java
@@ -30,7 +30,7 @@ public class make_panel3 implements ActionListener
 	
 	static final JPanel allchoice = new JPanel(new GridLayout(1, 2, -150, 0)); // 버튼 + 리스트 
 	static final JPanel List = new JPanel(new FlowLayout(FlowLayout.LEFT, 200, 25)); // 리스트
-	static final JPanel allstat = new JPanel(new GridLayout(2, 1)); // 버튼 + 리스트 + 파이차트
+	static JPanel allstat = new JPanel(new GridLayout(2, 1)); // 버튼 + 리스트 + 파이차트
 
 	// 사용하는 변수 선언 & 초기화
 	static jPanel03 jP3 = new jPanel03(); //파넬 전체
@@ -38,6 +38,7 @@ public class make_panel3 implements ActionListener
 	static ChartPanel PieChart; // 파이차트 
 	static JList list = new JList();// 리스트
 
+	//gender에서 사용하는 변수
 	static Float[][] gdarrdata;
 	static XYSeries gdseries1;
 	static XYSeries gdseries2 ;
@@ -46,19 +47,22 @@ public class make_panel3 implements ActionListener
 	static ChartPanel m_PieChart ;
 	static ChartPanel f_PieChart;
 	static double[][] gdfever_per; 
+	
 	// list에 들어갈 내용들
 	static String[] season = { "Spring", "Summer", "Fall", "Winter" };
 	static String[] age = { "0~9", "10~19", "20~29", "30~39", "40~49", "50~59", "60~69", "70~79" };
 	static String[] gender = {"Male","Female"};
-	
+	static String[] area = {"Seoul","Busan","Daegu","Gwangju","Incheon","Daejeon","Ulsan","Gyeonggi-do","Gangwon-do","Chungcheongbuk-do","Chungcheongnam-do","jeollabuk-do","jeollanam-do","Gyeongsangbuk-do","Gyeongsangnam-do","Jeju-do"};
 	static Dimension ld = new Dimension(130, 190); // list 크기 조절 
-	
+	static Dimension sd = new Dimension(145, 190);
 	private static JFrame jfjf ;
 	
 	public static JPanel make(JFrame JF)
 	{
 		jfjf = JF;
 		// -----------버튼만들기---------------------//
+		
+		//---------------------------전체 ------------------------------------//
 				final JButton button1 = new JButton("All");
 				
 				button1.addActionListener(new ActionListener(){
@@ -108,6 +112,8 @@ public class make_panel3 implements ActionListener
 						panel.removeAll();
 						allstat.removeAll();
 						
+						allstat = new JPanel(new GridLayout(2, 1));
+						
 						//리스트 목록 지우기//
 						List.removeAll();
 						list = new JList();
@@ -123,19 +129,22 @@ public class make_panel3 implements ActionListener
 					}
 				});
 
-				final JButton button2 = new JButton("Season");
-				button2.setActionCommand("Season");
-				button2.addActionListener(new ActionListener(){
+				
+				//---------------------------계절별 ------------------------------------//
+				final JButton button5 = new JButton("Season");
+				button5.setActionCommand("Season");
+				button5.addActionListener(new ActionListener(){
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						CP3 = new ChartPanel(null);
-						PieChart = new ChartPanel(null);
-						
 						//바뀔것은 미리 지우기
 						panel.removeAll();
 						List.removeAll();
 						panel.removeAll();
 						allstat.removeAll();
+						
+						allstat = new JPanel(new GridLayout(2, 1));
+						CP3 = new ChartPanel(null);
+						PieChart = new ChartPanel(null);
 						
 						// 리스트에 목록들 추가하기
 						list = new JList();
@@ -180,6 +189,8 @@ public class make_panel3 implements ActionListener
 					}
 				});
 
+				
+				//---------------------------성별 별 ------------------------------------//
 				final JButton button3 = new JButton("Gender");
 				button3.setActionCommand("Gender");
 				button3.addActionListener(new ActionListener(){
@@ -190,8 +201,10 @@ public class make_panel3 implements ActionListener
 						//원래 있던내용 비우기
 						panel.removeAll();
 						allstat.removeAll();
+						
 						PieChart = new ChartPanel(null);
 						CP3 = new ChartPanel(null);
+						allstat = new JPanel(new GridLayout(2, 1));
 						
 						try {
 							gdarrdata = database_load_gender.dload_avg();
@@ -224,7 +237,6 @@ public class make_panel3 implements ActionListener
 						List.add(list);
 						
 						allstat.add(allchoice);
-						
 						
 						//리스트 이벤트 발생!
 						//list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -283,7 +295,6 @@ public class make_panel3 implements ActionListener
 										}
 									}
 									
-									
 									panel.add(CP3);
 									panel.add(allstat);
 									jfjf.setVisible(true);
@@ -297,6 +308,7 @@ public class make_panel3 implements ActionListener
 					}
 				});
 				
+				//---------------------------연령대별 ------------------------------------//
 				final JButton button4 = new JButton("Age");
 				button4.setActionCommand("Age");
 				button4.addActionListener(new ActionListener(){
@@ -305,6 +317,7 @@ public class make_panel3 implements ActionListener
 						// TODO Auto-generated method stub
 						CP3 = new ChartPanel(null);
 						PieChart = new ChartPanel(null);
+						allstat = new JPanel(new GridLayout(2, 1));
 						
 						//바꿀거 지우기
 						panel.removeAll();
@@ -372,19 +385,86 @@ public class make_panel3 implements ActionListener
 					}
 				});
 
-				Dimension d = new Dimension(150, 80);
-				button1.setPreferredSize(d);
-				button2.setPreferredSize(d);
-				button3.setPreferredSize(d);
-				button4.setPreferredSize(d);
+				//---------------------------지역별 ------------------------------------//
+				final JButton button2 = new JButton("Area");
+				button2.setActionCommand("Area");
+				button2.addActionListener(new ActionListener(){
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						//바뀔것은 미리 지우기
+						panel.removeAll();
+						List.removeAll();
+						panel.removeAll();
+						allstat.removeAll();
+						
+						allstat = new JPanel(new GridLayout(2, 1));
+						CP3 = new ChartPanel(null);
+						PieChart = new ChartPanel(null);
+						
+						// 리스트에 목록들 추가하기
+						list = new JList();
+						//list.setPreferredSize(ld); // 크기 조절 
+						list.setListData(area); // 나이 목록 부르기
+						//List.add(list); // 파넬에 추가
+						
+						JScrollPane sc = new JScrollPane(list);
+						sc.setPreferredSize(sd);
+						
+						List.add(sc);
+						list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+						list.addListSelectionListener(new ListSelectionListener(){
+							@Override
+							public void valueChanged(ListSelectionEvent e) {
+								// TODO Auto-generated method stub
+								//if(list.getSelectedIndex() == 0)
+									System.out.println(list.getSelectedValue());
+									
+									if(list.getSelectedValue().equals(season[0])) // 봄
+									{
+										
+									}
+									else if(list.getSelectedValue().equals(season[1]))//여름
+									{
+										
+									}
+									else if(list.getSelectedValue().equals(season[2]))//가을
+									{
+										
+									}
+									else if(list.getSelectedValue().equals(season[3]))//겨울
+									{
+										
+									}
+							}
+						});
+						
+						//바뀐 내용들 추가하여 나타내기
+						allstat.add(allchoice);
+						allstat.add(PieChart);
 
+						panel.add(CP3);
+						panel.add(allstat);
+						jfjf.setVisible(true);
+					}
+				});
+				
+				
+				Dimension d1 = new Dimension(150, 80);
+				Dimension d2 = new Dimension(100, 80);
+				button1.setPreferredSize(d1);
+				button2.setPreferredSize(d1);
+				button3.setPreferredSize(d2);
+				button4.setPreferredSize(d2);
+				button5.setPreferredSize(d2);
+				
 				final JPanel buttonPanel1 = new JPanel((new FlowLayout(FlowLayout.LEFT, 50, 5)));
 				buttonPanel1.add(button1);
 				buttonPanel1.add(button2);
 
-				final JPanel buttonPanel2 = new JPanel((new FlowLayout(FlowLayout.LEFT, 50, 5)));
+				final JPanel buttonPanel2 = new JPanel((new FlowLayout(FlowLayout.LEFT, 37, 5)));
 				buttonPanel2.add(button3);
 				buttonPanel2.add(button4);
+				buttonPanel2.add(button5);
 
 				final JPanel Button = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 20));
 				Button.add(buttonPanel1);
@@ -411,7 +491,6 @@ public class make_panel3 implements ActionListener
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 	
 }
